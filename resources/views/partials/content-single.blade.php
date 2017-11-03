8cac52117719e933bb695dd39874fdf0d6f1f950
Suz Campbell <suz.blandon@gmail.com>
2017-11-02 20:56:29 -0600

    image gallery added to blog post

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -4,10 +4,24 @@
     @include('partials/entry-meta')
   </header>
   <div class="entry-content">
-    @php(the_content())
+    <div class="grid-x">
+      @php($images = get_field('image_gallery'))
+      <div class="cell {{ $images ? 'large-8' : '' }}">
+        @php(the_content())
+      </div>
+
+      @if ($images)
+        @php($size = 'large')
+        <div class="cell large-4">
+          @foreach ($images as $image)
+            {!! wp_get_attachment_image( $image['ID'], $size ) !!}
+          @endforeach
+        </div>
+      @endif
+    </div>
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php(comments_template('/partials/comments.blade.php'))
+  {{-- @php(comments_template('/partials/comments.blade.php')) --}}
 </article>
