25c8c6552cbd6c18507d0d42cc6e6d09ffc7ba54
Suz Campbell <suz.blandon@gmail.com>
2017-11-06 11:56:50 -0700

    added a little moon as the favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/x-icon" href="/wp-content/uploads/2017/11/pinkmoonfav.png" />
   @php(wp_head())
 </head>
