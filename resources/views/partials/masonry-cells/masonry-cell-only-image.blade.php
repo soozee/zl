cd2fcf7d2b85feb1e632b23d46720de617256656
Suz Campbell <suz.blandon@gmail.com>
2017-11-11 11:50:51 -0700

    fixing home page

--- resources/views/partials/masonry-cells/masonry-cell-only-image.blade.php
+++ resources/views/partials/masonry-cells/masonry-cell-only-image.blade.php
@@ -1,4 +1,4 @@
-@php ($image = get_sub_field('only-image'))
+@php ($image = get_sub_field('plain_image'))
 @if( $image )
     {!! wp_get_attachment_image( $image, 'large') !!}
 @endif
--- resources/views/partials/masonry-cells/masonry-cell-plain_image.blade.php
+++
@@ -1,4 +0,0 @@
-@php ($image = get_sub_field('plain_image'))
-@if( $image )
-    {!! wp_get_attachment_image( $image, 'large') !!}
-@endif
